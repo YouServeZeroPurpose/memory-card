4210c3d8c145f796d579547eef4e9c5eac388f6f
YouServeZeroPurpose <alexsolonskiy@gmail.com>
2024-09-17 21:21:08 +0300

    hi

--- card.py
+++ card.py
@@ -1,38 +1,67 @@
 from PyQt5.QtWidgets import QApplication, QWidget
+from random import shuffle
 
 app = QApplication([])
 
 from layout_quiz import *
+from questions import *
+
+i = 0
 
 window = QWidget()
 window.resize(600, 500)
 window.setWindowTitle('Memory Card')
 window.setLayout(main_line_quiz)
 window.show()
+shuffle(questions)
+questions[i].show_q(quest_lb, rb1, rb2, rb3, rb4)
 
 def click_ok():
+    global i
     if btn_ans.text() == 'Відповісти':
         rbGroupBox.hide()
         ansGroupBox.show()
+        right_ans_lb.setText(rb1.text())
         btn_ans.setText('Наступне питання')
+        if rb1.isChecked():
+            result_lb.setText('Правильно')
+        else:
+            result_lb.setText('Неправильно')
     else:
         rbGroupBox.show()
         ansGroupBox.hide()
         btn_ans.setText('Відповісти')
+        i += 1
+        try:
+            questions[i].show_q(quest_lb, rb1, rb2, rb3, rb4)
+        except:
+            app.exit()# prob do a counter here or some shi
+
     
 btn_ans.clicked.connect(click_ok)
 
 menu = QApplication([])
 menu_window = QWidget()
-menu_window.resize(200, 100)
+menu_window.resize(300, 150)
 menu_window.setWindowTitle('Меню')
 menu_window.setLayout(menu_main_line)
 
+window.hide()
+menu_window.show()
+
 def menu_act():
     menu_window.show()
+    window.hide()
+
+def menu1_act():
+    window.show()
+    menu_window.hide()
+
+def exit():
+    app.exit()
 
 btn_menu.clicked.connect(menu_act)
-
-
+menu_btn1.clicked.connect(menu1_act)
+menu_btn3.clicked.connect(exit)
 
 app.exec()
--- layout_quiz.py
+++ layout_quiz.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import (QPushButton, QRadioButton, QLabel, QSpinBox, QGroupBox, QButtonGroup, QHBoxLayout, QVBoxLayout)
+from PyQt5.QtWidgets import (QPushButton, QRadioButton, QLabel,
+                              QSpinBox, QGroupBox, QButtonGroup,
+                                QHBoxLayout, QVBoxLayout)
 from PyQt5.QtCore import Qt
 
 btn_menu = QPushButton('Меню')
@@ -8,14 +10,14 @@ spin.setValue(30)
 
 menu_main_line = QVBoxLayout()
 
-menu_btn1 = QPushButton('щось')
-menu_btn2 = QPushButton('щось інше')
-menu_btn3 = QPushButton('ще щось')
+menu_btn1 = QPushButton('Розпочати тестування')
+menu_btn2 = QPushButton('Розпочати навчання')
+menu_btn3 = QPushButton('Вийти')
 menu_main_line.addWidget(menu_btn1)
 menu_main_line.addWidget(menu_btn2)
 menu_main_line.addWidget(menu_btn3)
 
-quest_1b = QLabel('Яблуко')
+quest_lb = QLabel('Яблуко')
 rbGroupBox = QGroupBox('Варіанти відповіді')
 btn_ans = QPushButton('Відповісти')
 button_group = QButtonGroup()
@@ -45,7 +47,7 @@ rbGroupBox.setLayout(main_box_line)
 ansGroupBox = QGroupBox('Результати теста')
 main_box2_line = QVBoxLayout()
 result_lb = QLabel('Правильно')
-right_ans_lb = QLabel('apple')
+right_ans_lb = QLabel()
 main_box2_line.addWidget(result_lb, alignment=Qt.AlignTop)
 main_box2_line.addWidget(right_ans_lb, alignment=Qt.AlignCenter)
 ansGroupBox.setLayout(main_box2_line)
@@ -60,7 +62,7 @@ line1_quiz.addWidget(spin)
 line1_quiz.addWidget(QLabel('хвилин'))
 
 main_line_quiz.addLayout(line1_quiz, stretch=1)
-main_line_quiz.addWidget(quest_1b, alignment=Qt.AlignCenter, stretch=1)
+main_line_quiz.addWidget(quest_lb, alignment=Qt.AlignCenter, stretch=1)
 main_line_quiz.addWidget(rbGroupBox, stretch=5)
 main_line_quiz.addWidget(ansGroupBox, stretch=5)
 main_line_quiz.addWidget(btn_ans, stretch=1)
